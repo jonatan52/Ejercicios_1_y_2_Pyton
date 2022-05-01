b0d9a0ee10e5a0d53f81889e36e3cca4b37ca6e3
jonatan52 <tarriba52@gmail.com>
2022-05-01 16:27:11 -0500

    Ejercicio 1 y 2 python

---
+++ Ejercicio 1/ejercicio_1.py
@@ -0,0 +1,7 @@
+# Ejercicio 1
+# Autor: Jonatan Tarriba
+
+print("""
+         Hola, soy Jonatan
+         Estoy empezando el curso de python
+         Espero aprender mucho """)
---
+++ Ejercicio 2/ejercicio_2.py
@@ -0,0 +1,5 @@
+# Ejercicio 2
+# Autor: Jonatan Tarriba
+
+saludo = "Hola Mundo!"
+print(saludo)
